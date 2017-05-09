841179860c108c96ccbaea18dedfd41f3705b4e4
Martin Aceto <martin.aceto@gmail.com>
2017-05-09 15:42:49 -0300

    refactor on client

--- lib/slybroadcast/client.rb
+++ lib/slybroadcast/client.rb
@@ -29,9 +29,7 @@ module Slybroadcast
     ENDPOINTS = {
       verify: 'vmb.php',
       campaign_call_status: 'vmb.php',
-      campaign_pause: 'vmb.php',
-      campaign_resume: 'vmb.php',
-      campaign_cancel: 'vmb.php',
+      campaign: 'vmb.php',
       account_message_balance: 'vmb.php',
       download_audio_file: 'vmb.dla.php',
       list_audio_files: 'vmb.aflist.php'
@@ -47,86 +45,66 @@ module Slybroadcast
 
     def verify(**options)
       params = set_credentials(options.merge(c_option: :user_verify))
-      res = Net::HTTP.post_form(
-        endpoint_url,
-        set_credentials(options.merge(c_option: :user_verify))
-      )
+      res = make_post(endpoint_url, params)
       raise Exceptions::InvalidCredentials, 'Invalid `c_uid` or `c_password`' unless res.body.eql?("OK")
       true
     end
 
     def campaign_call_status(**options)
       params = set_credentials(options)
+      res = make_post(endpoint_url, params)
+      parse_response(:CampaignStatusResponse, res)
+    end
 
-      res = Net::HTTP.post_form(
-        endpoint_url,
-        params
-      )
-      result = Parsers::CampaignStatusResponse.new(res.body)
-      result.success? ? result : handle_error(result.error)
+    def campaign(**options)
+      params = set_credentials(options)
+      res = make_post(endpoint_url, params)
+      parse_response(:CampaignActionsResponse, res)
     end
 
     def campaign_pause(**options)
-      params = set_credentials(options.merge(c_option: 'pause'))
-      res = Net::HTTP.post_form(
-        endpoint_url,
-        params
-      )
-      result = Parsers::CampaignActionsResponse.new(res.body)
-      result.success? ? result : handle_error(result.error)
+      campaign(options.merge(c_option: :pause))
     end
 
     def campaign_resume(**options)
-      params = set_credentials(options.merge(c_option: 'run'))
-      res = Net::HTTP.post_form(
-        endpoint_url,
-        params
-      )
-      result = Parsers::CampaignActionsResponse.new(res.body)
-      result.success? ? result : handle_error(result.error)
+      campaign(options.merge(c_option: :run))
     end
 
     def campaign_cancel(**options)
-      params = set_credentials(options.merge(c_option: 'stop'))
-      res = Net::HTTP.post_form(
-        endpoint_url,
-        params
-      )
-      result = Parsers::CampaignActionsResponse.new(res.body)
-      result.success? ? result : handle_error(result.error)
+      campaign(options.merge(c_option: :stop))
     end
 
     def account_message_balance
       params = set_credentials(remain_message: '1')
-      res = Net::HTTP.post_form(
-        endpoint_url,
-        params
-      )
-      result = Parsers::RemainingMessagesResponse.new(res.body)
-      result.success? ? result : handle_error(result.error)
+      res = make_post(endpoint_url, params)
+      parse_response(:RemainingMessagesResponse, res)
     end
 
     def download_audio_file(**options)
       params = set_credentials(options)
-      res = Net::HTTP.post_form(
-        endpoint_url,
-        params
-      )
-      result = Parsers::RemainingMessagesResponse.new(res.body)
-      result.success? ? result : handle_error(result.error)
+      res = make_post(endpoint_url, params)
+      parse_response(:RemainingMessagesResponse, res)
     end
 
     def list_audio_files
       params = set_credentials(c_method: 'get_audio_list')
-      res = Net::HTTP.post_form(
-        endpoint_url,
-        params
-      )
-      result = Parsers::AudioFileListResponse.new(res.body)
-      result.success? ? result : handle_error(result.error)
+      res = make_post(endpoint_url, params)
+      parse_response(:AudioFileListResponse, res)
     end
 
     private
+
+    def make_post(url, params)
+      Net::HTTP.post_form(
+        url,
+        params
+      )
+    end
+
+    def parse_response(parser, response)
+      result = Parsers.const_get(parser).new(response.body)
+      result.success? ? result : handle_error(result.error)
+    end
 
     def endpoint_url
       method = caller_locations(1,1).first.label.to_sym
